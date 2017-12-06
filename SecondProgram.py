ae93fc6447ade692f151ff2fc965295c5ced2494
kanchanj3000 <kanchan.j3000@gmail.com>
2017-12-06 14:01:58 +0530

    My second proogram Commit

--- SecondProgram.py
+++ SecondProgram.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome("C:\Users\Friends\PycharmProjects\selenium\drivers\chromedriver.exe")
-driver.get("http:\\www.myntra.com")
+driver.get("http:\\www.mynttra.com")
 driver.quit()
