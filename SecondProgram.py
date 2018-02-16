dd9e5504343273102db0cf6976c18fca5343d4a8
kanchanj3000 <34279101+kanchanj3000@users.noreply.github.com>
2018-02-16 14:24:17 +0530

    Delete SecondProgram.py

--- SecondProgram.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome("C:\Users\Friends\PycharmProjects\selenium\drivers\chromedriver.exe")
-driver.get("http:\\www.mynttra.com")
-driver.quit()
